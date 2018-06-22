de4a3dbf6e9ec5dee47ab73e638cc0549b25077d
RawTables <danilopc@protonmail.com>
2018-06-22 18:26:25 -0300

    Fixed dirb problem
    Fixed the issue with dirb

--- recon_enum/reconscan.py
+++ recon_enum/reconscan.py
@@ -97,7 +97,7 @@ def write_to_file(ip_address, enum_type, data):
 
 def dirb(ip_address, port, url_start, wordlist="/usr/share/wordlist/dirb/big.txt, /usr/share/wordlist/dirb/vulns/cgis.txt"):
     print bcolors.HEADER + "INFO: Starting dirb scan for " + ip_address + bcolors.ENDC
-    DIRBSCAN = "dirb %s://%s:%s %s -o ../reports/%s/dirb-%s.txt -r" % (url_start, ip_address, port, ip_address, ip_address, wordlist)
+    DIRBSCAN = "dirb %s://%s:%s -o ../reports/%s/dirb-%s.txt -r" % (url_start, ip_address, port, ip_address, ip_address, wordlist)
     print bcolors.HEADER + DIRBSCAN + bcolors.ENDC
     results_dirb = subprocess.check_output(DIRBSCAN, shell=True)
     print bcolors.OKGREEN + "INFO: RESULT BELOW - Finished with dirb scan for " + ip_address + bcolors.ENDC
